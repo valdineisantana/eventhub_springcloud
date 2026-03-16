c5c60b4f68eddb8d4d19a466477d3e08bc843444
valdineisantana <valdinei.santana@uninove.edu.br>
2026-03-16 11:58:38 -0300

    Align monitoring consumer bean with 'monitoring' binding for debit-credit-monitoring hub

--- src/main/java/com/rabobank/gainit/debitcreditmonitoring/consumer/EventMonitoringConfig.java
+++ src/main/java/com/rabobank/gainit/debitcreditmonitoring/consumer/EventMonitoringConfig.java
@@ -14,23 +14,23 @@ import java.util.function.Consumer;
 public class EventMonitoringConfig {
 
     @Bean
-    public Consumer<Message<String>> monitorEvents() {
+    public Consumer<Message<String>> monitoring() {
         return message -> {
-            log.info("Received debit/credit event: {}", message.getPayload());
-            log.info("Event headers - Partition Key: {}, Sequence Number: {}, Offset: {}, Enqueued Time: {}",
+            log.info("Received monitoring event from debit-credit-monitoring: {}", message.getPayload());
+            log.info("Monitoring event headers - Partition Key: {}, Sequence Number: {}, Offset: {}, Enqueued Time: {}",
                      message.getHeaders().get(AzureHeaders.PARTITION_KEY),
                      message.getHeaders().get("x-opt-sequence-number"),
                      message.getHeaders().get("x-opt-offset"),
                      message.getHeaders().get("x-opt-enqueued-time"));
             Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(AzureHeaders.CHECKPOINTER);
             if (checkpointer != null) {
-                log.info("Starting manual checkpoint for event: {}", message.getPayload());
+                log.info("Starting manual checkpoint for monitoring event: {}", message.getPayload());
                 checkpointer.success()
-                        .doOnSuccess(success -> log.info("Event '{}' successfully checkpointed", message.getPayload()))
-                        .doOnError(error -> log.error("Error checkpointing event", error))
+                        .doOnSuccess(success -> log.info("Monitoring event '{}' successfully checkpointed", message.getPayload()))
+                        .doOnError(error -> log.error("Error checkpointing monitoring event", error))
                         .block();
             } else {
-                log.warn("Checkpointer not found in message headers for event: {}", message.getPayload());
+                log.warn("Checkpointer not found in message headers for monitoring event: {}", message.getPayload());
             }
         };
     }
