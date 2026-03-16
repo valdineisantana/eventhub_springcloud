6fbf00cc18370ab0dfde2e9fca3d5c3f616fd225
valdineisantana <valdinei.santana@uninove.edu.br>
2026-03-16 11:38:16 -0300

    Implement manual checkpointing for all Event Hub consumers

--- src/main/java/com/rabobank/gainit/debitcreditmonitoring/EventController.java
+++ src/main/java/com/rabobank/gainit/debitcreditmonitoring/EventController.java
@@ -1,16 +1,16 @@
 package com.rabobank.gainit.debitcreditmonitoring;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class EventController {
 
-    @Autowired
-    private StreamBridge streamBridge;
+    private final StreamBridge streamBridge;
 
     @PostMapping("/send-event")
     public String sendEvent(@RequestBody String event) {
--- src/main/java/com/rabobank/gainit/debitcreditmonitoring/EventMonitoringConfig.java
+++ src/main/java/com/rabobank/gainit/debitcreditmonitoring/EventMonitoringConfig.java
@@ -1,22 +1,57 @@
 package com.rabobank.gainit.debitcreditmonitoring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.azure.spring.messaging.AzureHeaders;
+import com.azure.spring.messaging.checkpoint.Checkpointer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 
 import java.util.function.Consumer;
 
 @Configuration
+@Slf4j
 public class EventMonitoringConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(EventMonitoringConfig.class);
+    @Bean
+    public Consumer<Message<String>> monitorEvents() {
+        return message -> {
+            log.info("Received debit/credit event: {}", message.getPayload());
+            Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(AzureHeaders.CHECKPOINTER);
+            if (checkpointer != null) {
+                checkpointer.success()
+                        .doOnSuccess(success -> log.info("Event '{}' successfully checkpointed", message.getPayload()))
+                        .doOnError(error -> log.error("Error checkpointing event", error))
+                        .block();
+            }
+        };
+    }
 
     @Bean
-    public Consumer<String> monitorEvents() {
-        return event -> {
-            logger.info("Received debit/credit event: {}", event);
-            // Add monitoring logic here, e.g., parse event, check thresholds, send alerts, etc.
+    public Consumer<Message<String>> monitorCredit() {
+        return message -> {
+            log.info("Received credit process event: {}", message.getPayload());
+            Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(AzureHeaders.CHECKPOINTER);
+            if (checkpointer != null) {
+                checkpointer.success()
+                        .doOnSuccess(success -> log.info("Credit event '{}' successfully checkpointed", message.getPayload()))
+                        .doOnError(error -> log.error("Error checkpointing credit event", error))
+                        .block();
+            }
+        };
+    }
+
+    @Bean
+    public Consumer<Message<String>> monitorDebit() {
+        return message -> {
+            log.info("Received debit process event: {}", message.getPayload());
+            Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(AzureHeaders.CHECKPOINTER);
+            if (checkpointer != null) {
+                checkpointer.success()
+                        .doOnSuccess(success -> log.info("Debit event '{}' successfully checkpointed", message.getPayload()))
+                        .doOnError(error -> log.error("Error checkpointing debit event", error))
+                        .block();
+            }
         };
     }
 }
